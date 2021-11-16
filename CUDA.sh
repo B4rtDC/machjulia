419ee14654817076c81216261e143228edc09b42
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 20:56:16 +0100

    update

--- CUDA.sh
+++ CUDA.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --partition=GPU          # partition to use
-#SBATCH --gpus=1
+
 
 
 echo $CUDA_VISIBLE_DEVICES
