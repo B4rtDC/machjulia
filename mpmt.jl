804980dd5b26228cde46b9b1c6c9342a331a1411
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 21:20:14 +0100

    added multiprocessing

---
+++ mpmt.jl
@@ -0,0 +1,28 @@
+using Distributed
+using Logging
+
+# Logging setup
+io = open("mpmt_log.txt", "w+")
+mylogger = Logging.SimpleLogger(io)
+
+# read settings
+num_workers = parse(Int, ENV["SLURM_NTASKS"])
+num_threads = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
+
+
+# create workers
+addprocs(num_workers, env=["JULIA_NUM_THREADS"=>num_threads])
+with_logger(mylogger) do
+    println("Number of cores: ", nprocs())
+    println("Number of workers: ", nworkers())
+
+    for i in workers()
+        id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
+        println(id, " " , pid, " ", host)
+    end
+end
+
+
+# Cleanup
+flush(io)
+close(io)
---
+++ mpmt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#
+#SBATCH --partition 80CORE 
+#SBATCH --nodes 2 # two nodes
+#SBATCH --ntasks 4 # 4 process in total(worker)
+#SBATCH --cpus-per-task 5 # 5 threads per task => 40 in total
+
+: '
+detail idea
+'
+
+echo "Starting job!!! ${SLURM_JOB_ID}"
+
+# Config 
+julia_path="$HOME" # where to place julia
+script_path="$HOME/machjulia"
+
+# Run script
+$julia_path/julia-1.6.3/bin/julia $script_path/mpmt.jl
+
+
+
