03381d4721fcdc820ba4828720248fe0893ef2fa
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 21:35:44 +0100

    update

--- mpmt.jl
+++ mpmt.jl
@@ -1,9 +1,4 @@
 using Distributed
-using Logging
-
-# Logging setup
-io = open("mpmt_log.txt", "a+")
-
 
 # read settings
 num_workers = parse(Int, ENV["SLURM_NTASKS"])
@@ -13,23 +8,13 @@ num_threads = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
 # create workers
 addprocs(num_workers)#, :env=["JULIA_NUM_THREADS"=>num_threads])
 
-@everywhere begin
-    using Logging
-    mylogger = Logging.SimpleLogger(io)
+
+println("Number of cores: ", nprocs())
+println("Number of workers: ", nworkers())
+
+for i in workers()
+    # get description of everyone
+    id, pid, host, tr = fetch(@spawnat i (myid(), getpid(), gethostname(), Threads.nthreads()))
+    println(id, " " , pid, " ", host, " ", tr)
+    #@info "worker id: $(id), process id: $(pid), host: $(host)"
 end
-
-with_logger(mylogger) do
-    println("Number of cores: ", nprocs())
-    println("Number of workers: ", nworkers())
-
-    for i in workers()
-        id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
-        println(id, " " , pid, " ", host)
-        @info "worker id: $(id), process id: $(pid), host: $(host)"
-    end
-end
-
-
-# Cleanup
-flush(io)
-close(io)
