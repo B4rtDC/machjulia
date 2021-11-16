813380f5efc75ed09dfffe00e65ed7b66ec43c7b
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 21:27:17 +0100

    update

--- mpmt.jl
+++ mpmt.jl
@@ -19,6 +19,7 @@ with_logger(mylogger) do
     for i in workers()
         id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
         println(id, " " , pid, " ", host)
+        @info "worker id: $(id), process id: $(pid), host: $(host)"
     end
 end
 
