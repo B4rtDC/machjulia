f8a5312c5b1f6fb2a1c7e21da8709b5b7f8940fe
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 20:55:08 +0100

    update cuda

--- CUDA.sh
+++ CUDA.sh
@@ -4,7 +4,10 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --partition=GPU          # partition to use
-#SBATCH --gres=gpu:1             # number of gpus per node
+#SBATCH --gpus=1
+
+
+echo $CUDA_VISIBLE_DEVICES
 
 # Config 
 julia_path="$HOME" # where to place julia
