a2627a65df10c561335cb7df38e261d22865db9e
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 21:28:36 +0100

    logger update

--- mpmt.jl
+++ mpmt.jl
@@ -3,7 +3,7 @@ using Logging
 
 # Logging setup
 io = open("mpmt_log.txt", "w+")
-mylogger = Logging.SimpleLogger(io)
+
 
 # read settings
 num_workers = parse(Int, ENV["SLURM_NTASKS"])
@@ -12,6 +12,9 @@ num_threads = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
 
 # create workers
 addprocs(num_workers)#, :env=["JULIA_NUM_THREADS"=>num_threads])
+
+@everywhere mylogger = Logging.SimpleLogger(io)
+
 with_logger(mylogger) do
     println("Number of cores: ", nprocs())
     println("Number of workers: ", nworkers())
