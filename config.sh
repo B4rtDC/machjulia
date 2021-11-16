90918feb0cb4e9d7069b0ab4608cd661e3544948
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 18:48:34 +0100

    update

--- config.sh
+++ config.sh
@@ -1,20 +1,17 @@
 # config file to get you started
-
-# Dwnload latest julia version
 original_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # folder of script
 download_path="$HOME/downloads" # where to download
 julia_path="$HOME" # where to place julia
-#mkdir -p $download_path
+
+# Dwnload latest julia version
+mkdir -p $download_path
 cd $download_path
 echo "Downloading in <$download_path>"
-
-# get latest julia build and extract
-#wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.3-linux-x86_64.tar.gz
-#tar zxvf julia-1.6.3-linux-x86_64.tar.gz
-#mv "$download_path/julia-1.6.3" "$julia_path"
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.3-linux-x86_64.tar.gz
+tar zxvf julia-1.6.3-linux-x86_64.tar.gz
+mv "$download_path/julia-1.6.3" "$julia_path"
 
 # run julia and install required packages
-echo "planned line: $julia_path/julia-1.6.3/bin/julia $original_path/config.jl"
-
 $julia_path/julia-1.6.3/bin/julia $original_path/config.jl
-# 
+
+echo "Ready to work!"
