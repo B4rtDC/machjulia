cf0e6990faabb06125048221e2bd479b973af315
Bart De Clerck <declerck.bart@pm.me>
2021-11-17 00:14:51 +0100

    update

--- mpmt.jl
+++ mpmt.jl
@@ -7,8 +7,8 @@ num_threads = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
 
 # create workers
 #addprocs_slurm(num_workers, env=["JULIA_NUM_THREADS"=>num_threads])
-addprocs(SlurmManager(num_workers))#, cpus_per_task="$(num_threads)")
-
+#addprocs(SlurmManager(num_workers))#, cpus_per_task="$(num_threads)")
+addprocs_slurm(num_workers)
 @everywhere begin
     function myfun(i)
         println("iteration $(i) on worker $(myid()) on host $(gethostname())")
