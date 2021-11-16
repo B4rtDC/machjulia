3e67099ea6b63396e3c9958e9f929f2d43161710
Bart De Clerck <declerck.bart@pm.me>
2021-11-16 22:50:53 +0100

    update

--- mpmt.jl
+++ mpmt.jl
@@ -1,20 +1,33 @@
 using Distributed
-
+using ClusterManagers
 # read settings
 num_workers = parse(Int, ENV["SLURM_NTASKS"])
 num_threads = parse(Int, ENV["SLURM_CPUS_PER_TASK"])
 
 
 # create workers
-addprocs(num_workers)#, :env=["JULIA_NUM_THREADS"=>num_threads])
+addprocs(SlurmManager(num_workers), env=["JULIA_NUM_THREADS"=>num_threads])
 
+@everywhere begin
+    function myfun(i)
+        println("iteration $(i) on worker $(myid()) on host $(gethostname())")
+        sleep(10)
+    end
+end
 
 println("Number of cores: ", nprocs())
 println("Number of workers: ", nworkers())
 
+#=
 for i in workers()
     # get description of everyone
     id, pid, host, tr = fetch(@spawnat i (myid(), getpid(), gethostname(), Threads.nthreads()))
     println(id, " " , pid, " ", host, " ", tr)
+    myfun(i)
     #@info "worker id: $(id), process id: $(pid), host: $(host)"
+end
+=#
+
+@parallel for i = 1:400
+    myfun(i)
 end
