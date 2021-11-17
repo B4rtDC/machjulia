50e8deb731148d6867f1fab07436f8122e42d702
Bart De Clerck <declerck.bart@pm.me>
2021-11-17 18:15:49 +0100

    update multithreading script

--- mt.jl
+++ mt.jl
@@ -7,7 +7,7 @@ mylogger = Logging.SimpleLogger(io)
 # Actual code
 with_logger(mylogger) do
     @info "Multithreading demo:\n → Available threads: $(Threads.nthreads())"
-    Threads.@threads for i in 1:20
+    Threads.@threads for i in 1:100
         @info "\n\titeration $(i) on thread $(Threads.threadid())\n" 
     end
 end
--- mt.sh
+++ mt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #
 #SBATCH --partition 80CORE 
-#SBATCH --nodes 1 # single node
+#SBATCH --nodes=1 # single node
 #SBATCH --ntasks=1 # single process (worker)
 #SBATCH --cpus-per-task=17 # 17 threads
-
+#SBATCH --mem=64g 
 : '
 Using simple multithreading on a single blade
 
@@ -19,13 +19,6 @@ echo "Starting job!!! ${SLURM_JOB_ID}"
 # Config 
 julia_path="$HOME" # where to place julia
 script_path="$HOME/machjulia"
-original_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # folder of script
-
-echo "julia path: $julia_path"
-echo "original path: $original_path"
-
-
-julia -e 'println("\n"); [println((k,ENV[k],)) for k in keys(ENV) if occursin("SLURM",k)]; println("\n");'
 
 # Run script with amount of threads
 $julia_path/julia-1.6.3/bin/julia --threads ${SLURM_CPUS_PER_TASK} $script_path/mt.jl
