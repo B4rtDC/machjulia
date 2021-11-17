30d8d6f27d7a2813fbf39280e8fc9e6b798e60c5
Bart De Clerck <declerck.bart@pm.me>
2021-11-17 18:20:52 +0100

    update CUDA demo

--- CUDA_demo.jl
+++ CUDA_demo.jl
@@ -7,7 +7,8 @@ When trying to do work on only one GPU, the example below appears to work
 =#
 
 using Distributed, CUDA
-
+println(device())
+println([CUDA.capability(dev) for dev in CUDA.devices()])
 
 # single proces, using all four GPUs
 @sync begin
@@ -29,43 +30,3 @@ using Distributed, CUDA
         #println(typeof(b)," ", b)
     end
 end
-
-#=
-# add single worker process
-addprocs(1)
-@everywhere using CUDA
-worker = workers()[1]
-device = collect(devices())[1]
-
-# run task on single device
-remotecall_wait(device) do
-    device!(device)
-    println("worker $(worker) uses device $(device)")
-end
-=#
-
-# assign devices
-# asyncmap((zip(workers(), devices()))) do (p, d)
-#     remotecall_wait(p) do
-#         @info "Worker $p uses $d"
-#         device!(d)
-#     end
-# end
-# Logging setup
-#io = open("CUDA_log.txt", "w+")
-#mylogger = Logging.SimpleLogger(io)
-
-#=
-# Actual code
-with_logger(mylogger) do
-    @info CUDA.versioninfo()
-    @info [CUDA.capability(dev) for dev in CUDA.devices()]
-end
-=#
-
-#println(CUDA.versioninfo())
-println([CUDA.capability(dev) for dev in CUDA.devices()])
-
-# Cleanup
-#flush(io)
-#close(io)
