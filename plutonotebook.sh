aec063930f8660974829e42e5f395cc1772d9a0e
Bart De Clerck <declerck.bart@pm.me>
2021-11-17 17:27:49 +0100

    update notebook

--- plutonotebook.sh
+++ plutonotebook.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=pluto-test
+#SBATCH --job-name=pluto
 #SBATCH --nodes=1
-#SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --partition=GPU          # use GPU partition or CORE80 or ...
+#SBATCH --ntasks=1               # total number of tasks (workers) across all nodes
+#SBATCH --cpus-per-task=27        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --partition=CORE80          # use GPU partition or CORE80 or ...
 #SBATCH --mem=16g 
 
 # Config 
@@ -13,6 +13,8 @@ script_path="$HOME/machjulia"
 port=3300 # port for notebook server
 
 # forward port from actual notebook server machine to login server
+# This does not work for the DGX cluster :-( => why?
+# gives following error: Host key verification failed.
 /usr/bin/ssh -N -f -R $port:localhost:$port mach.intra.rma.ac.be
 
 
