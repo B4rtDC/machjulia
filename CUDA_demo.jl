8fcb3c12045c3db6f15117a176ca9763f7ab5152
Bart De Clerck <declerck.bart@pm.me>
2021-11-17 13:26:51 +0100

    update

--- CUDA_demo.jl
+++ CUDA_demo.jl
@@ -1,5 +1,32 @@
-using CUDA, Logging
+#=
+The DGX station contains 4 Nvidia Tesla V100 cards with 32Gb of RAM on each of them. 
+Because there are multiple cards, getting it to work appears to be a bit tricky.
 
+When trying to do work on only one GPU, the example below appears to work
+
+=#
+
+using Distributed, CUDA
+
+# add single worker process
+addprocs(1)
+@everywhere using CUDA
+worker = workers()[1]
+device = devices()[1]
+
+# run task on single device
+remotecall_wait(device) do
+    device!(device)
+    println("worker $(worker) uses device $(device)")
+end
+
+# assign devices
+# asyncmap((zip(workers(), devices()))) do (p, d)
+#     remotecall_wait(p) do
+#         @info "Worker $p uses $d"
+#         device!(d)
+#     end
+# end
 # Logging setup
 #io = open("CUDA_log.txt", "w+")
 #mylogger = Logging.SimpleLogger(io)
