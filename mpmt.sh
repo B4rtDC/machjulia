832da8762b4ec10734d6370402252f599895d2fd
Bart De Clerck <declerck.bart@pm.me>
2021-11-22 10:21:01 +0100

    fixed typo

--- mpmt.sh
+++ mpmt.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition 80CORE 
 #SBATCH --nodes 2 # two nodes
 #SBATCH --ntasks 6 # 4 process in total(worker)
-#SBATCH --cpus-per-task 10 # 5 threads per task => 40 in total
+#SBATCH --cpus-per-task 10 # 10 threads per task => 40 in total
 export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 : '
